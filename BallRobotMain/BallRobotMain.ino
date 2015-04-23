cc631e8b52c2ffd85128be8d908c095e793e37e8
Upas Narayan <unaraya1@jhu.edu>
2015-04-23 16:04:12 -0400

    averaging for pixy, collecting balls is working

--- BallRobotMain/BallRobotMain.ino
+++ BallRobotMain/BallRobotMain.ino
@@ -22,14 +22,14 @@ const int WALL_SIG = 4;
 //NOTE: AVOID USING PINS 50-53 (FOR MEGA). These pins are ICSP and will be used up by the PixyCam.
 //NOTE: AVOID USING PINS 10-13 (FOR UNO).
 // output pins for drive wheels
-const int LEFT_WHEEL_PIN_1 = 6;
-const int LEFT_WHEEL_PIN_2 = 7;
-const int RIGHT_WHEEL_PIN_1 = 8;
+const int LEFT_WHEEL_PIN_1 = 3;
+const int LEFT_WHEEL_PIN_2 = 5;
+const int RIGHT_WHEEL_PIN_1 = 6;
 const int RIGHT_WHEEL_PIN_2 = 9;
 
 // output pins for brush
 const int BRUSH_PIN_1 = 4;
-const int BRUSH_PIN_2 = 5;
+const int BRUSH_PIN_2 = 2;
 
 // pins for left/right swithes to determine if hitting walls
 const int LEFT_SWITCH_PIN = 2;
@@ -46,6 +46,8 @@ const unsigned int SCORE_TIME = 7000;
 // time to backup and turn for avoiding wall
 const unsigned int WALL_BACKUP_TIME = 2000;
 const float WALL_TURN_TIME = 1000;
+
+const int SPEED = 200;
 
 int numBallsCollected = 0;
 unsigned long startTime = millis();
@@ -64,38 +66,40 @@ void setup() {
 }
 
 void loop() {
+  
+  turnRobotOff();
+  delay(500);
+  
+  //float observations = numObservations(GAME_BALL_SIG);
+  //Serial.println(observations);
+  Block* average = getAverageBlock(GAME_BALL_SIG);
 
-  Block* block = getMaxBlock(GAME_BALL_SIG);
+  if (average != NULL) {
 
-  if (block != NULL) {
-
-    //Serial.print(block->width);
-    //Serial.print(block->height);
-    
-    Serial.print(block->x);
+    Serial.print(average->x);
     Serial.print(" ");
     
-    if ( block->x > MAX_WIDTH / 3.0 && block->x < (2 / 3.0 * MAX_WIDTH)) {
+    if ( average->x > MAX_WIDTH / 3.0 && average->x < (2 / 3.0 * MAX_WIDTH)) {
       // Go straight if ball is in the middle third
       turnRobotForward();
       turnBrushForward();
       Serial.println("Moving forward");
-      delay(500);
+      delay(1000);
     }
-    else if ( block->x < MAX_WIDTH / 3.0) {
+    else if ( average->x < MAX_WIDTH / 3.0) {
 
       // Turn right if ball is in the leftmost third of its vision
       turnRobotLeft();
-        turnBrushOff();
+      turnBrushOff();
       Serial.println("Turning left");
-      delay(100);
+      delay(200);
     }
-    else if (block->x > (2 / 3.0 * MAX_WIDTH)){
+    else if (average->x > (2 / 3.0 * MAX_WIDTH)){
       // Turn left if ball is in the rightmost third
       turnRobotRight();
-          turnBrushOff();
+      turnBrushOff();
       Serial.println("Turning right");
-      delay(100);
+      delay(200);
     }
 /*
     // Spin brush if ball is reasonably close
@@ -110,6 +114,7 @@ void loop() {
     rotateRobot();
     turnBrushOff();
     Serial.println("Rotating robot");
+    delay(100);
   }
   /*
   // Turn towards goal if timed out
@@ -137,6 +142,40 @@ void loop() {
   //turnBrushForward();
 
   //testMotorFunctions();
+}
+
+float numObservations(int inputSig) {
+  int count = 0;
+  for (int i = 0; i < 5; i++) {
+    Block* block = getMaxBlock(inputSig);
+    if (block != NULL) {
+      Serial.println("got here");
+      count++;
+    }
+  }
+  return count / 5.0;
+}
+
+Block* getAverageBlock(int inputSig) {
+  float totalx = 0.0;
+  float totalwidth = 0.0;
+  float totalheight = 0.0;
+  float count = 0;
+  Block *avgBlock = new Block();
+  for (int i = 0; i < 5; i++) {
+    Block* block = getMaxBlock(inputSig);
+    if (block != NULL) {
+      totalx += block->x;
+      totalwidth += block->width;
+      totalheight += block->height;
+      count++;
+    }
+  }
+  if (count == 0) return NULL;
+  avgBlock->x = totalx / count;
+  avgBlock->width = totalwidth / count;
+  avgBlock->height = totalheight / count;
+  return avgBlock;
 }
 
 Block* getMaxBlock(int inputSig) {
@@ -205,10 +244,11 @@ void scoreBalls() {
     turnRobotForward();
   }
   // rotate until robot finds goal again
-  while (getMaxBlock(GOAL_SIG) == NULL) {
+  /*while (getMaxBlock(GOAL_SIG) == NULL) {
     rotateRobot();
-  }
+  }*/
   // turn brush back to score ball
+  turnRobotOff();
   int scoreStartTime = millis();
   while (millis() - scoreStartTime <= SCORE_TIME) {
     turnBrushBack();
@@ -216,38 +256,45 @@ void scoreBalls() {
 }
 
 void turnRobotLeft() {
-  digitalWrite(LEFT_WHEEL_PIN_1, LOW);
-  digitalWrite(LEFT_WHEEL_PIN_2, HIGH);
-  digitalWrite(RIGHT_WHEEL_PIN_1, HIGH);
-  digitalWrite(RIGHT_WHEEL_PIN_2, LOW);
+  analogWrite(LEFT_WHEEL_PIN_1, LOW);
+  analogWrite(LEFT_WHEEL_PIN_2, SPEED);
+  analogWrite(RIGHT_WHEEL_PIN_1, SPEED);
+  analogWrite(RIGHT_WHEEL_PIN_2, LOW);
 }
 
 void turnRobotRight() {
-  digitalWrite(LEFT_WHEEL_PIN_1, HIGH);
-  digitalWrite(LEFT_WHEEL_PIN_2, LOW);
-  digitalWrite(RIGHT_WHEEL_PIN_1, LOW);
-  digitalWrite(RIGHT_WHEEL_PIN_2, HIGH);
+  analogWrite(LEFT_WHEEL_PIN_1, SPEED);
+  analogWrite(LEFT_WHEEL_PIN_2, LOW);
+  analogWrite(RIGHT_WHEEL_PIN_1, LOW);
+  analogWrite(RIGHT_WHEEL_PIN_2, SPEED);
 }
 
 void turnRobotForward() {
-  digitalWrite(LEFT_WHEEL_PIN_1, HIGH);
-  digitalWrite(LEFT_WHEEL_PIN_2, LOW);
-  digitalWrite(RIGHT_WHEEL_PIN_1, HIGH);
-  digitalWrite(RIGHT_WHEEL_PIN_2, LOW);
+  analogWrite(LEFT_WHEEL_PIN_1, SPEED);
+  analogWrite(LEFT_WHEEL_PIN_2, LOW);
+  analogWrite(RIGHT_WHEEL_PIN_1, SPEED);
+  analogWrite(RIGHT_WHEEL_PIN_2, LOW);
 }
 
 void turnRobotBack() {
-  digitalWrite(LEFT_WHEEL_PIN_1, LOW);
-  digitalWrite(LEFT_WHEEL_PIN_2, HIGH);
-  digitalWrite(RIGHT_WHEEL_PIN_1, LOW);
-  digitalWrite(RIGHT_WHEEL_PIN_2, HIGH);
+  analogWrite(LEFT_WHEEL_PIN_1, LOW);
+  analogWrite(LEFT_WHEEL_PIN_2, SPEED);
+  analogWrite(RIGHT_WHEEL_PIN_1, LOW);
+  analogWrite(RIGHT_WHEEL_PIN_2, SPEED);
+}
+
+void turnRobotOff() {
+  analogWrite(LEFT_WHEEL_PIN_1, LOW);
+  analogWrite(LEFT_WHEEL_PIN_2, LOW);
+  analogWrite(RIGHT_WHEEL_PIN_1, LOW);
+  analogWrite(RIGHT_WHEEL_PIN_2, LOW);
 }
 
 void rotateRobot() {
-  digitalWrite(LEFT_WHEEL_PIN_1, LOW);
-  digitalWrite(LEFT_WHEEL_PIN_2, HIGH);
-  digitalWrite(RIGHT_WHEEL_PIN_1, HIGH);
-  digitalWrite(RIGHT_WHEEL_PIN_2, LOW);
+  analogWrite(LEFT_WHEEL_PIN_1, LOW);
+  analogWrite(LEFT_WHEEL_PIN_2, SPEED);
+  analogWrite(RIGHT_WHEEL_PIN_1, SPEED);
+  analogWrite(RIGHT_WHEEL_PIN_2, LOW);
 }
 
 void turnBrushForward() {
